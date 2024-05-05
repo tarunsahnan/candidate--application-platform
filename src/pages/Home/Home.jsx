8f6fabe7160c06240de79c60f50d0f558f0ccd46
Tarun Sahnan <tarunsahnan98@gmail.com>
2024-05-05 20:49:37 +0530

    calculate filtered data updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,59 +5,30 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchData } from "../../store/actions/fetchJobActions";
 import Filters from "../../components/filters/Filters";
 
+const initialFilterState = {
+  minExperience: [],
+  companyName: "",
+  location: [],
+  remoteOnSite: [],
+  role: [],
+  minBasePay: [],
+};
+
 const Home = () => {
   const dispatch = useDispatch();
   const { loading, data } = useSelector((state) => state.fetchJob);
   const [pageNumber, setPageNumber] = useState(0);
   const [loadingMore, setLoadingMore] = useState(false);
   const [filteredData, setFilteredData] = useState([]);
-  const [filters, setFilters] = useState();
+  const [filters, setFilters] = useState(initialFilterState);
 
   const onFilterChange = (filters) => {
-    console.log({ filters });
     setFilters(filters);
-    const newData = data.filter((job) => {
-      if (filters.minExperience) {
-        if (job.minExp && job.minExp < filters.minExperience) {
-          return false;
-        } else if (!job.minExp) {
-          return false;
-        }
-      }
-      if (
-        filters.companyName.length &&
-        !job.companyName.toLowerCase().includes(filters.companyName)
-      )
-        return false;
-      if (filters.location.length && !filters.location.includes(job.location))
-        return false;
-
-      if (filters.remoteOnSite.length === 1) {
-        if (
-          filters.remoteOnSite.includes("Remote") &&
-          job.location !== "remote"
-        )
-          return false;
-        if (
-          filters.remoteOnSite.includes("On-site") &&
-          job.location === "remote"
-        )
-          return false;
-      }
-
-      if (filters.role.length && !filters.role.includes(job.jobRole))
-        return false;
-      if (filters.minBasePay) {
-        if (!job.minJdSalary && job.maxJdSalary < filters.minBasePay)
-          return false;
-        else if (job.minJdSalary && job.minJdSalary < filters.minBasePay)
-          return false;
-      }
-      return true;
-    });
-    console.log({ data, newData });
-    setFilteredData(newData);
   };
+
+  useEffect(() => {
+    console.log({ filteredData });
+  }, [filteredData]);
 
   const pageSize = 10;
 
@@ -87,14 +58,79 @@ const Home = () => {
     // console.log({ totalCount, i: pageNumber * pageSize });
     // if (totalCount && pageNumber * pageSize >= totalCount) return;
     if (!loading && !loadingMore) {
+      // setPageNumber(pageNumber + 1);
       setLoadingMore(true);
-      setPageNumber((prev) => prev + 1);
     }
   };
 
   useEffect(() => {
     if (!loading) setLoadingMore(false);
   }, [loading]);
+
+  useEffect(() => {
+    if (data) {
+      const calculateFilteredData = () => {
+        console.log({ filters });
+        const isFilterAdded =
+          filters.minExperience.length !== 0 ||
+          filters.companyName !== "" ||
+          filters.location.length !== 0 ||
+          filters.remoteOnSite.length !== 0 ||
+          filters.role.length !== 0 ||
+          filters.minBasePay.length !== 0;
+        if (!isFilterAdded) {
+          setFilteredData(data);
+          return;
+        }
+        const newData = data.filter((job) => {
+          if (filters.minExperience) {
+            if (job.minExp && job.minExp < filters.minExperience) {
+              return false;
+            } else if (!job.minExp) {
+              return false;
+            }
+          }
+          if (
+            filters.companyName.length &&
+            !job.companyName.toLowerCase().includes(filters.companyName)
+          )
+            return false;
+          if (
+            filters.location.length &&
+            !filters.location.includes(job.location)
+          )
+            return false;
+
+          if (filters.remoteOnSite.length === 1) {
+            if (
+              filters.remoteOnSite.includes("Remote") &&
+              job.location !== "remote"
+            )
+              return false;
+            if (
+              filters.remoteOnSite.includes("On-site") &&
+              job.location === "remote"
+            )
+              return false;
+          }
+
+          if (filters.role.length && !filters.role.includes(job.jobRole))
+            return false;
+          if (filters.minBasePay) {
+            if (!job.minJdSalary && job.maxJdSalary < filters.minBasePay)
+              return false;
+            else if (job.minJdSalary && job.minJdSalary < filters.minBasePay)
+              return false;
+          }
+          return true;
+        });
+        console.log({ data, newData });
+        setFilteredData(newData);
+      };
+
+      calculateFilteredData(filters);
+    }
+  }, [data, filters]);
 
   return (
     <Box display="flex" flexDirection="column" gap="60px">
